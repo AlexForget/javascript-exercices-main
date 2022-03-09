0bfcad212a3c53e2f0763d078117545a0b79ddd4
AlexForget <alexandreqc26@gmail.com>
2022-03-09 09:02:59 -0500

    Complete exercices 9 et 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = (string) => {
-  return palindromes(string);
+const palindromes = (oldString) => {
+  oldString = oldString.replace(/[^a-zA-Z]/g, "").toLowerCase();
+
+  var newString = "";
+  for (var i = oldString.length - 1; i >= 0; i--) {
+    newString += oldString[i];
+  }
+
+  return newString !== oldString ? false : true;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = (number) => {
+  let a = 0;
+  let b = 1;
 
+  if (number < 0) return "OOPS";
+
+  for (let i = 1; i < number; i++) {
+    let temp = b;
+    b = a + b;
+    a = temp;
+  }
+  return b;
 };
 
 // Do not edit below this line
